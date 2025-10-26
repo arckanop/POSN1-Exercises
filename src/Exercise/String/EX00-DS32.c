ea5d24d3e31cd10832c90ef04ab53c144748e07e
Kanop Sutharomna <78390700+arckanop@users.noreply.github.com>
2025-10-26 21:53:38 +0700

    Add string exercise 0

---
+++ src/Exercise/String/EX00-DS32.c
@@ -0,0 +1,11 @@
+//Kanop Sutharomna
+
+#include <stdio.h>
+
+int main() {
+    char name[50], surname[50];
+    scanf("%s %s", name, surname);
+
+    printf("%s\n%s", name, surname);
+    return 0;
+}
